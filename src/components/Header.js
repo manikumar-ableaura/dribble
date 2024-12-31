98f0ad91857328b4eacec6e326267dc18114ebc7
Mani kumar <manikumar@ableaura.com>
2024-12-31 13:25:01 +0530

    msg dribble

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+// src/components/Header.js
+import React from 'react';
+import './Header.css';
+
+function Header() {
+  return (
+    <header>
+      <h1 className='header'>Hello folks!</h1>
+    </header>
+  );
+}
+
+export default Header;
